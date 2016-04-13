490a38a24cddde1402df4772e1ea286af0782122
javie <javie@JAVIER-CASA>
2016-04-13 20:38:56 +0200

    CAMBIOS
    -Cambios en el comparator del priotityQueue

--- Sokoban/src/motor/MyComparator.java
+++ Sokoban/src/motor/MyComparator.java
@@ -5,15 +5,24 @@ import java.util.Comparator;
 public class MyComparator implements Comparator<Node>{
 	
 	public int compare(Node a, Node b) {
-		if(a.getI().compareTo(b.getI())!=0)
+		if(a.getI()!=b.getI())
 		{//primero los nodos con más cajas acomodadas correctamente i(n)
-			return (-1)*(a.getI().compareTo(b.getI()));
+			if(a.getI()>b.getI())
+			{
+				return -1;
+			}else{
+				return 1;
+			}
 
 		}else
 		{// en caso de empates seguirán aquellos con menor costo acumulado de g(n) y h(n)
 			Integer aux = a.getG()+a.getH();
 			Integer aux2 = b.getG()+b.getH();
-			return aux.compareTo(aux2);
+			if(aux<aux2){
+				return -1;
+			}else{
+				return 1;
+			}
 		}
 	}
 }
