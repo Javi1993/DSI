00dd18b417c70dabb55c3ab555bcb78c6fb4ff75
dsi <dsi@dsi-VirtualBox>
2016-04-24 01:03:23 +0200

    CAMBIOS
    -Cambios en la clase Simulacion.java

--- Sokoban/src/motor/Simulacion.java
+++ Sokoban/src/motor/Simulacion.java
@@ -10,7 +10,7 @@ public class Simulacion {
 	public static void main(String[] args) throws IOException{
 		Mapas.generarMapas();
 		Player p = new Player("User", "pass");
-		while(p.getProgreso()<151)
+		while(p.getProgreso()<=150)
 		{
 			Escenario testEscenario = new Escenario(p.getProgreso(), false);//creamos el escenario
 			if(Resolver.solucion(testEscenario, 0)==null){
@@ -20,25 +20,5 @@ public class Simulacion {
 			System.out.println("Nivel "+p.getProgreso()+" completado!");
 			p.updatePlayer(null, testEscenario);
 		}
-
-
-		//Ver cajas por nivel
-		//		File file = new File("C:\\Users\\javie\\Desktop\\filename.txt");
-		//		if (!file.exists()) {
-		//			file.createNewFile();
-		//		}
-		//		FileWriter fw = new FileWriter(file.getAbsoluteFile());
-		//		BufferedWriter bw = new BufferedWriter(fw);
-		//
-		//		Mapas.generarMapas();
-		//		Player p = new Player("User", "pass");
-		//		while(p.getProgreso()<151)
-		//		{
-		//			Escenario testEscenario = new Escenario(p.getProgreso(), false);//creamos el escenario
-		//			bw.write("Nivel "+p.getProgreso()+": "+testEscenario.cajas());
-		//			bw.newLine();
-		//			p.updatePlayer(null, testEscenario);
-		//		}
-		//		bw.close();
 	}
 }
