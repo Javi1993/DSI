baf3c1e551c61eb215d2db08facf07a7fa416949
javie <javie@JAVIER-CASA>
2016-04-27 12:28:22 +0200

    CAMBIOS
    -Añadido opción de 'Enter' en panel login

--- Sokoban/src/interfaz/Login.java
+++ Sokoban/src/interfaz/Login.java
@@ -7,12 +7,16 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import jugador.Player;
@@ -20,10 +24,13 @@ import jugador.Player;
 @SuppressWarnings("serial")
 public class Login extends JFrame{
 
-	JTextField user;
-	JPasswordField password;
+	private JTextField user;
+	private JPasswordField password;
 	Player player;
 	boolean valido;
+	private JButton b1;
+	private static Action enterAction; 
+
 
 	public Login(){
 		this.setTitle("DSI | Sokoban");
@@ -53,14 +60,12 @@ public class Login extends JFrame{
 	private void crearBotones(){
 		JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 15));
 		southPanel.setOpaque(true);
-		JButton b1 = new JButton("Login"); 
-		b1.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
+		b1 = new JButton("Login"); 
+
+		b1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
 				Border borde = new LineBorder(Color.RED, 1);
-				if(user.getText().trim().length()==0||password.getPassword().length==0)
-				{
+				if(user.getText().trim().length()==0||password.getPassword().length==0){
 					valido = false;
 				}else{
 					player = new Player(user.getText().trim(), new String(password.getPassword()));
@@ -75,10 +80,19 @@ public class Login extends JFrame{
 				}
 			}
 		});
+		enterAction = new EnterAction();
+		password.getInputMap().put( KeyStroke.getKeyStroke("ENTER"), "doEnterAction" ); 
+		password.getActionMap().put( "doEnterAction", enterAction );
 
 		b1.setPreferredSize(new Dimension(90, 25));
 		b1.setFont(new Font("Arial", 1, 11));
 		southPanel.add(b1);
 		this.add(southPanel, BorderLayout.SOUTH);
+	}
+
+	class EnterAction extends AbstractAction{
+		public void actionPerformed( ActionEvent tf ) { 
+			b1.doClick(); 
+		}  
 	}
 }
